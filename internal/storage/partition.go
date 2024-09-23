2539f9647f1d2bbadc8131df44fe06a224194240
Tosin <akinnagbelast@gmail.com>
2024-09-23 13:56:31 +0100

    logical partition

--- internal/storage/partition.go
+++ internal/storage/partition.go
@@ -12,14 +12,16 @@ import (
 )
 
 type PartitionConfig struct {
-	dir string
+	dir            string
+	maxIdxSizeByte uint64
+	maxMsgSizeByte uint64
 }
 
 type Partition struct {
 	topic           string
 	segments        []*Segment
 	writableSegment *Segment
-	config          PartitionConfig
+	PartitionConfig
 }
 
 func NewPartition(topic string, c PartitionConfig) (*Partition, error) {
@@ -33,29 +35,27 @@ func NewPartition(topic string, c PartitionConfig) (*Partition, error) {
 		return nil, err
 	}
 	p := &Partition{
-		config: c,
+		PartitionConfig: c,
+		topic:           topic,
 	}
 
 	sort.Slice(segments, func(i, j int) bool { return segments[i].Name() < segments[j].Name() })
 
-	for _, segment := range segments {
-		if segment.IsDir() {
-			continue
-		}
-
+	for i := 0; i < len(segments); i = i + 2 { // each segment has msg file and index file with same name. Makes sense to only get one of each.
+		segment := segments[i]
 		segName := strings.TrimSuffix(segment.Name(), path.Ext(segment.Name()))
 		if segName == "" {
 			return nil, fmt.Errorf("invalid segment name: %s", segment.Name())
 		}
 		startOffset, err := strconv.Atoi(segName)
 		if err != nil {
-			return nil, fmt.Errorf("invalid segment name. should be integer: %s", segment.Name())
+			return nil, fmt.Errorf("invalid segment name. should be an integer: %s", segment.Name())
 		}
 
-		s, err := NewSegment(filepath.Join(partitionDir, segName), config{
-			allowedIndexSize: 800,
-			allowedMsgSize:   1024,
-			startOffset:      uint32(startOffset),
+		s, err := NewSegment(partitionDir, segmentConfig{
+			maxIdxSizeByte: p.maxIdxSizeByte,
+			maxMsgSizeByte: p.maxMsgSizeByte,
+			startOffset:    uint32(startOffset),
 		})
 
 		if err != nil {
@@ -63,13 +63,15 @@ func NewPartition(topic string, c PartitionConfig) (*Partition, error) {
 		}
 
 		p.segments = append(p.segments, s)
+		p.writableSegment = s // Will assign last segment eventually as writable segment
 	}
 
 	if len(segments) == 0 { // indicates an empty partition
-		s, err := NewSegment(filepath.Join(partitionDir, "0"), config{
-			allowedIndexSize: 800,
-			allowedMsgSize:   1024,
-			startOffset:      uint32(0),
+		startOffset := uint32(0)
+		s, err := NewSegment(partitionDir, segmentConfig{
+			maxIdxSizeByte: p.maxIdxSizeByte,
+			maxMsgSizeByte: p.maxMsgSizeByte,
+			startOffset:    startOffset,
 		})
 
 		if err != nil {
@@ -79,17 +81,18 @@ func NewPartition(topic string, c PartitionConfig) (*Partition, error) {
 		p.writableSegment = s
 	}
 
-	return nil, nil
+	return p, nil
 }
 
 func (p *Partition) Append(message []byte) (uint32, error) {
 	off, err := p.writableSegment.Append(message)
 	if err != nil {
-		if err == io.EOF { // indicates a full segment and should add/update writable segment
-			s, err := NewSegment(filepath.Join(p.config.dir, fmt.Sprintf("%d", p.writableSegment.nextOffset)), config{
-				allowedIndexSize: 800,
-				allowedMsgSize:   1024,
-				startOffset:      p.writableSegment.nextOffset,
+		if err == io.EOF { // indicates a full segment and should create a new segment, then add/update writable segment
+
+			s, err := NewSegment(filepath.Join(p.dir, fmt.Sprintf("%d", p.writableSegment.nextOffset)), segmentConfig{
+				maxIdxSizeByte: p.maxIdxSizeByte,
+				maxMsgSizeByte: p.maxMsgSizeByte,
+				startOffset:    p.writableSegment.nextOffset,
 			})
 
 			if err != nil {
@@ -104,4 +107,35 @@ func (p *Partition) Append(message []byte) (uint32, error) {
 	}
 
 	return off, nil
+}
+
+func (p *Partition) Read(offset uint32) (message []byte, err error) {
+	segment := p.getOffsetSegment(offset)
+	if segment == nil {
+		return nil, fmt.Errorf("invalid offset: %d", offset)
+	}
+
+	return segment.Read(offset)
+}
+
+func (p *Partition) getOffsetSegment(offset uint32) *Segment {
+	for _, segment := range p.segments {
+		if offset >= segment.c.startOffset && offset < segment.nextOffset {
+			return segment
+		}
+	}
+	return nil
+}
+
+func (p *Partition) Name() string {
+	return filepath.Join(p.dir, fmt.Sprintf("part_%s", p.topic))
+}
+
+func (p *Partition) Close() error {
+	for _, s := range p.segments {
+		if err := s.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
