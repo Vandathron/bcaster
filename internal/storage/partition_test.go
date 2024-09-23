002b9d676ef6291af8d7cef9ca9581767fed9bc3
Tosin <akinnagbelast@gmail.com>
2024-09-23 23:06:02 +0100

    Test partition with multiple segments/writes/reads

--- internal/storage/partition_test.go
+++ internal/storage/partition_test.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"os"
 	"path/filepath"
@@ -14,7 +16,7 @@ func TestNewPartition(t *testing.T) {
 
 	topic := "customer_created"
 	config := PartitionConfig{
-		dir:            dir,
+		baseDir:        dir,
 		maxIdxSizeByte: 1024,
 		maxMsgSizeByte: 800,
 	}
@@ -35,11 +37,7 @@ func TestNewPartitionWithExistingSegments(t *testing.T) {
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
 	topic := "customer_created"
-	config := PartitionConfig{
-		dir:            dir,
-		maxIdxSizeByte: 1024,
-		maxMsgSizeByte: 800,
-	}
+	config := getPartitionConfig(dir)
 
 	partition, err := NewPartition(topic, config)
 	require.NoError(t, err)
@@ -54,4 +52,87 @@ func TestNewPartitionWithExistingSegments(t *testing.T) {
 	require.Equal(t, 1, len(partition.segments)) // Still one segment as it's potentially latest and writable segment
 	require.NotNil(t, partition.writableSegment)
 	require.NoError(t, partition.Close())
+}
+
+func TestPartition_BasicAppendRead(t *testing.T) {
+	dir, err := os.MkdirTemp("", "test_partition")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	topic := "customer_created"
+	config := getPartitionConfig(dir)
+
+	partition, err := NewPartition(topic, config)
+	require.NoError(t, err)
+	msgByte := getTestMsgByte(0)
+	require.NoError(t, err)
+	offset, err := partition.Append(msgByte)
+	require.NoError(t, err)
+	require.Equal(t, uint32(0), offset)
+	require.Equal(t, uint32(0), partition.writableSegment.c.startOffset)
+	require.Equal(t, 1, len(partition.segments))
+	msg, err := partition.Read(offset)
+	require.NoError(t, err)
+	require.Equal(t, msgByte, msg)
+	require.NoError(t, partition.Close())
+}
+
+func TestPartition_MultiWritesReads(t *testing.T) {
+	dir, err := os.MkdirTemp("", "test_partition")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	topic := "customer_created"
+	config := getPartitionConfig(dir)
+
+	partition, err := NewPartition(topic, config)
+	require.NoError(t, err)
+	segmentCnt := uint64(len(partition.segments))
+	require.Equal(t, uint64(1), segmentCnt)
+	currMsgFileSizeByte := uint64(0)
+
+	for i := 0; i < 500; i++ {
+		msgByte := getTestMsgByte(i + 1)
+		msgSizeByte := uint64(len(msgByte) + 8)
+		offset, err := partition.Append(msgByte)
+		require.NoError(t, err)
+		require.Equal(t, uint32(i), offset)
+		currMsgFileSizeByte += msgSizeByte // msgSizeByte varies
+		currIdxEntrySizeByte := indexEntryWidth * uint64(offset+1)
+		segmentLen := uint64(len(partition.segments))
+		if currMsgFileSizeByte > config.maxMsgSizeByte*segmentLen || currIdxEntrySizeByte > config.maxIdxSizeByte*segmentLen { // indicates maxed out segment, hence partition should add new segment
+			segmentCnt++
+			require.Equal(t, int(segmentCnt), len(partition.segments), fmt.Sprintf("offset: %d \n msgSize: %d totalMsgSize: %d, max: %d", offset, msgSizeByte, currMsgFileSizeByte, config.maxMsgSizeByte*segmentCnt))
+			require.Equal(t, partition.writableSegment, partition.segments[segmentCnt-1]) // latest segment should be the writable segment
+		}
+	}
+
+	for i := 0; i < 500; i++ {
+		msg, err := partition.Read(uint32(i))
+		require.NoError(t, err, fmt.Sprintf("offset: %d", i))
+		m := message{}
+		err = json.Unmarshal(msg, &m)
+		require.NoError(t, err)
+		require.Equal(t, i+1, m.Id)
+	}
+
+	require.NoError(t, partition.Close())
+}
+
+func getTestMsgByte(id int) []byte {
+	msgByte, err := json.Marshal(message{
+		Name:  "Van",
+		Event: "New customer",
+		Id:    id,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return msgByte
+}
+
+func getPartitionConfig(dir string) PartitionConfig {
+	return PartitionConfig{
+		baseDir:        dir,
+		maxIdxSizeByte: 1024 * 500,
+		maxMsgSizeByte: 1024 * 1024,
+	}
 }
