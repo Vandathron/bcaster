615f866fc4e7a9819b98b6e72c53a5bf37aaf25e
Tosin <akinnagbelast@gmail.com>
2024-10-16 16:02:58 +0100

    more tests for multi append read consumer

--- internal/storage/consumer_test.go
+++ internal/storage/consumer_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"github.com/stretchr/testify/require"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -44,5 +45,46 @@ func TestConsumer_AppendReadMultiple(t *testing.T) {
 	file, err := os.CreateTemp("", "con.consumer")
 	require.NoError(t, err)
 	defer file.Close()
-	defer
+	defer os.RemoveAll(file.Name())
+
+	c, err := NewConsumer(file.Name(), config{
+		maxSize: 1024 * 1024,
+	})
+	require.NoError(t, err)
+
+	for i := 0; i < 20; i++ {
+		id := []byte("new_server_" + strconv.Itoa(i))
+		pos, err := c.Append(id, uint64(i))
+		require.NoError(t, err)
+		require.Equal(t, uint32(i*consumerSize), pos)
+	}
+	require.NoError(t, c.Close())
+
+	// reopen file
+	c, err = NewConsumer(file.Name(), config{
+		maxSize: 1024 * 1024,
+	})
+	require.NoError(t, err)
+	for i := 0; i < 20; i++ {
+		idBytes, off, err := c.Read(uint32(i*consumerSize), true)
+		require.NoError(t, err)
+		expectedByte := []byte("new_server_" + strconv.Itoa(i))
+		require.Equal(t, expectedByte, idBytes)
+		require.Equal(t, uint64(i), off)
+	}
+	require.NoError(t, c.Close())
+
+	// reopen file
+	c, err = NewConsumer(file.Name(), config{
+		maxSize: 1024 * 1024,
+	})
+	require.NoError(t, err)
+
+	// re-read consumer, consumer next read offset should add by 1
+	for i := 0; i < 20; i++ {
+		_, off, err := c.Read(uint32(i*consumerSize), true)
+		require.NoError(t, err)
+		require.Equal(t, uint64(i+1), off)
+	}
+	require.NoError(t, c.Close())
 }
